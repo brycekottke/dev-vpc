69d87ba204008edf36fd2d47d366051ef01e3f50
Bryce Kottke <brycek@auctionedge.com>
2017-07-15 18:17:23 -0700

    Errors reading mapped vars

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "${var.global["vpc_cidr"]}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+}
+
+resource "aws_subnet" "internal" {
+  count             = "${length(split(",", var.subnet_int))}"
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "${element(split(",", var.global["subnets_int"]), count.index)}"
+  availability_zone = "${element(split(",", var.global["azs"]), count.index)}"
+  vars = {
+    subnet_int  = "${var.global["subnets_int"]}"
+  }
+
+}
