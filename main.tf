6ccea3c9ae7d86eea12ecf4e1f9ec1b1eb2a9826
Bryce Kottke <brycek@auctionedge.com>
2017-07-15 19:52:57 -0700

    Renamed variables.tfvars to variables.tf fixed issue. lol

--- main.tf
+++ main.tf
@@ -2,11 +2,12 @@ resource "aws_vpc" "main" {
   cidr_block           = "${var.global["vpc_cidr"]}"
   enable_dns_support   = true
   enable_dns_hostnames = true
-
 }
 
 resource "aws_subnet" "internal" {
-  count             = "${length(split(",", var.global["subnet_int"]))}"
+  count             = "${length(split(",", var.global["subnets_int"]))}"
+  #count             = "${length(lookup(var.global["subnets_int"], split(",")))}"
+  #count             = "${lookup(var.global["subnets_int"], split(",")))}"
   vpc_id            = "${aws_vpc.main.id}"
   cidr_block        = "${element(split(",", var.global["subnets_int"]), count.index)}"
   availability_zone = "${element(split(",", var.global["azs"]), count.index)}"
